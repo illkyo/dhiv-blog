b0dcb08478d8b1dcf188116d7f79ac6382f13a0f
4thjonasbrother <fakefrankcody8@gmail.com>
2024-06-17 07:10:25 +0500

    added user profile pics to be viewable in the home page on the posts

--- users/models.py
+++ users/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 from django.core import validators
 from django.utils.deconstruct import deconstructible
+from PIL import Image
 
 @deconstructible
 class DhivehiUsernameValidator(validators.RegexValidator):
@@ -52,7 +53,15 @@ class Profile(models.Model):
     def save(self, *args, **kwargs):
         if not self.image:
             self.set_default_pfp()
-        super().save(*args, **kwargs)
+            
+        super().save(*args, **kwargs)    
+        
+        img = Image.open(self.image.path)
+        
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
     
     def __str__(self):
         return f'{self.user.username} ގެ ޕްރޮފައިލް'
